81e6013c8fad3789c662bfd1c90d94ae0c9eb834
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-06-26 11:11:37 -0300

    Update build configurations and add Nativewind support

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:@react-native/babel-preset',
+    'nativewind/babel',
+  ],
+  plugins: [
+    'react-native-paper/babel',
+    ['module:react-native-dotenv'],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,11 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const nativeWindConfig = withNativeWind(defaultConfig, {
+  input: "./global.css",
+  projectRoot: __dirname,
+});
+
+module.exports = mergeConfig(defaultConfig, nativeWindConfig);
